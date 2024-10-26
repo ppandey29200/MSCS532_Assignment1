5aa1bcb3899805ff6645a6c7f14ba7122f9a5fc5
ppandey29200 <ppandey29200@ucumberlands.edu>
2024-10-26 09:17:11 +0545

    Proper comments on the code

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Insertion sort algorithm in decreasing order
 def decreasing_insertion_sort(arr):
+    # Traverse through the array size
     for j in range(1, len(arr)):
         key = arr[j]
         i = j - 1
+        # Compare the key with the previous elements
         while i >= 0 and key > arr[i]:
             arr[i + 1] = arr[i]
             i -= 1
+        # Insert the key at the correct position
         arr[i + 1] = key
     return arr
 
